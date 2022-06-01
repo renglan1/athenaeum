c1e4ca684df514fe633108bf79cc6f28e209c703
renglan1 <rob.eng@unb.ca>
2022-06-01 12:49:24 -0300

    refactored some code

--- app/scripts/fx/fx-load-background.js
+++ app/scripts/fx/fx-load-background.js
@@ -1,17 +1,64 @@
-const html = document.querySelector("html");
-const fxState = localStorage.getItem("fxState");
-console.log("fxState: " + fxState);
-
-const rootBackgroundImage = document.querySelector("img#root-background-image");
-const pageBackgroundImage = document.querySelector("img#page-background-image");
-
-// !!TEMP!!
 localStorage.setItem("bgName", "firelink");
 localStorage.setItem("bgQuality", "4k");
-bgName = localStorage.getItem("bgName");
+let bgName = localStorage.getItem("bgName");
 
-setFXClass(fxState);
-setImageBackground(rootBackgroundImage);
-setImageBackground(pageBackgroundImage);
-setVideoBackground(fxState, "div#", "root", "");
-setVideoBackground(fxState, "div#", "page", "-background-container");
+window.addEventListener("load", function(){
+    setFXClass(localStorage.getItem("fxState"));
+    setBackgrounds(localStorage.getItem("fxState"));
+});
+
+function setFXClass(fxState){
+    const html = document.querySelector("html");
+    
+    if(fxState === "on"){
+        html.classList.add("fx");
+        localStorage.setItem("fxState", "on");
+    }
+    else{
+        html.classList.remove("fx");
+        localStorage.setItem("fxState", "off");
+    }
+}
+
+function setBackgrounds(fxState){
+    const rootBackgroundImage = document.querySelector("img#root-background-image");
+    const pageBackgroundImage = document.querySelector("img#page-background-image");
+
+    setImageBackground(rootBackgroundImage);
+    setImageBackground(pageBackgroundImage);
+    setVideoBackground(fxState, "div#", "root", "");
+    setVideoBackground(fxState, "div#", "page", "-background-container");
+}
+
+function setVideoBackground(fxState, parentPrefix, parentName, parentSuffix) {
+    const parent = document.querySelector(`${parentPrefix}${parentName}${parentSuffix}`);
+    const bgName = localStorage.getItem("bgName");
+    const bgQuality = localStorage.getItem("bgQuality");
+
+    if (fxState === "on") {
+        const background = document.createElement("video");
+        background.className = "background";
+        background.id = `${parentName}-background-video`;
+
+        background.setAttribute("preload", "none");
+        background.muted = true;
+        background.loop = true;
+        background.autoplay = true;
+        background.src = `../res/videos/backgrounds/${bgQuality}/${bgName}.mp4`;
+
+        if(parent != null){
+            parent.insertBefore(background, parent.childNodes[0]);
+        }
+    }
+    else{
+        if(background = document.querySelector("video.background")){
+            background.remove();
+        } 
+    }
+}
+
+function setImageBackground(element){
+    if(element != null){
+        element.src = `../res/images/backgrounds/${bgName}.png`;
+    }
+}
---
+++ app/scripts/menus/settings-menu/background-select/background-select-generate.js
@@ -0,0 +1,4 @@
+const backgroundSelectOptions = document.querySelector("div#background-select-options");
+const backgroundNames = ["abyss", "astral", "embers", "firelink", "neptune", "pan", "tiamat"];
+
+console.log(backgroundSelectOptions);
