cda6f6a4031bed29778638ce4252afa148d7d295
renglan1 <rob.eng@unb.ca>
2022-06-01 15:29:33 -0300

    preview background on hover works as intended

--- app/scripts/menus/settings-menu/background-select/background-select-generate.js
+++ app/scripts/menus/settings-menu/background-select/background-select-generate.js
@@ -1,9 +1,9 @@
 localStorage.setItem("bgName", "firelink");
 localStorage.setItem("bgQuality", "4k");
 
-//window.addEventListener("load", appendBackgroundOptions(bgNames, localStorage.getItem("fxState")));
+window.addEventListener("load", appendBgOptions(bgNames, localStorage.getItem("fxState")));
 
-function appendBackgroundOptions(bgNames, fxState){
+function appendBgOptions(bgNames, fxState){
     const bgSelectOptions = document.querySelector("div#background-select-options");
     const curBg = localStorage.getItem("bgName");
 
@@ -20,15 +20,71 @@ function createBgSelectOption(fxState, bgName, bgQuality){
     bgSelectOption.className = "background-select-option";
     bgSelectOption.id = bgName;
 
+    const bgSelectOptionPreview = createBgSelectOptionPreview(fxState, bgName, bgQuality);
+
+    bgSelectOption.appendChild(bgSelectOptionPreview);
+
+    return bgSelectOption;
+}
+
+function createBgSelectOptionPreview(fxState, bgName, bgQuality){
+    const bgSelectOptionPreview = document.createElement("div");
+    bgSelectOptionPreview.className = "background-select-option-preview";
+    const bgSelectOptionTitle = createBgSelectOptionTitle(bgName);
+    bgSelectOptionPreview.appendChild(bgSelectOptionTitle);
+
     const bgSelectOptionImage = document.createElement("img");
     bgSelectOptionImage.src = `${bgImagesPath}/${bgName}.png`;
-    bgSelectOption.appendChild(bgSelectOptionImage);
+    bgSelectOptionPreview.appendChild(bgSelectOptionImage);
 
     if(fxState === "on"){
         const bgSelectOptionVideo = document.createElement("video");
+
+        bgSelectOptionVideo.muted = true;
+        bgSelectOptionVideo.loop = true;
         bgSelectOptionVideo.src = `${bgVideosPath}/${bgQuality}/${bgName}.mp4`;
-        bgSelectOption.appendChild(bgSelectOptionVideo);
+        
+        bgSelectOptionPreview.appendChild(bgSelectOptionVideo);
+
+        bgSelectOptionPreview.addEventListener("mouseenter", function(){
+            bgSelectOptionVideo.play().then(function(){
+                setGrayscale(bgSelectOptionVideo, "off");
+                bgSelectOptionTitle.style.width = "65%";
+            });
+        });
+
+        bgSelectOptionPreview.addEventListener("mouseleave", function(){
+            bgSelectOptionVideo.pause();
+            setGrayscale(bgSelectOptionVideo, "on");
+            bgSelectOptionTitle.style.width = "60%";
+        });
     }
 
-    return bgSelectOption;
+    return bgSelectOptionPreview;
+}
+
+function createBgSelectOptionTitle(bgName){
+    const bgSelectOptionTitle = document.createElement("div");
+    bgSelectOptionTitle.className = "background-select-option-title";
+
+    for(const letter of bgName){
+        if(letter != null){
+            const span = document.createElement("span");
+            span.textContent = letter;
+            bgSelectOptionTitle.appendChild(span);
+        }
+    }
+
+    return bgSelectOptionTitle;
+}
+
+function setGrayscale(element, state){
+    if(state === "on"){
+        element.style.filter = "grayscale(100%)";
+        element.style.webkitFilter = "grayscale(100%)";
+    }
+    else{
+        element.style.filter = "grayscale(0%)";
+        element.style.webkitFilter = "grayscale(0%)";
+    }
 }
